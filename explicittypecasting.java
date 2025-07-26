c2cd53765fea074385ca7e9ffa18f7472253669e
samikshadesai922 <Samiksha.2403284210@vcet.edu.in>
2025-07-26 12:26:45 +0530

    Add files via upload
    Explicit Typecasting converts integer value into double value

---
+++ explicittypecasting.java
@@ -0,0 +1,11 @@
+public class Main{
+	public static void main(String[] args){
+	
+	double db = 7.89;
+	int result = (int)db;
+	 
+	System.out.println(" double " + db);
+	System.out.println(" Explicit type casting " + result);
+	
+     }
+}
