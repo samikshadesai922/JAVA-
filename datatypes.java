351d41f1d3bd43b973fde58dc34c699973603a61
samikshadesai922 <Samiksha.2403284210@vcet.edu.in>
2025-07-26 12:09:54 +0530

    Add files via upload
    Basic data types of java

---
+++ datatypes.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 25;
+        double b = 3.14;
+        char ch = 'A';
+        boolean c = true;
+        String s = "Hello Java!";
+
+        System.out.println("Integer: " + a);
+        System.out.println("Double: " + b);
+        System.out.println("Char: " + ch);
+        System.out.println("Boolean: " + c);
+        System.out.println("String: " + s);
+    }
+}
+
