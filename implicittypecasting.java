2aa321eb7b75cec0996f08c3ee6ef9fb420ef3d9
samikshadesai922 <Samiksha.2403284210@vcet.edu.in>
2025-07-26 12:13:37 +0530

    Add files via upload
    implicit type casting converts integer into double

---
+++ implicittypecasting.java
@@ -0,0 +1,13 @@
+public class Main{
+	public static void main(String[] args){
+	
+	int num = 10;
+	
+	double db = num;
+	
+	System.out.println(" Integer " + num );
+	System.out.println(" Type casting " + db);
+
+  	}
+}
+	
